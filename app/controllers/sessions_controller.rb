24da623f3378165c0db2c78dcde9c60f206db5a6
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-09-09 09:04:48 -0700

    commit tree 4196860ddede38c8780a1dc39cb1f86178b65876

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   
   def index
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:where_am_i] ||= "Where are you?"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
